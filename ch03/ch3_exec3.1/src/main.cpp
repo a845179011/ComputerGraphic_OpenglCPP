983acf1fb09fc48692cfffa96e32683e440304d1
rzh <845179011@qq.com>
2023-04-17 22:53:39 +0800

    习题3.1

---
+++ ch03/ch3_exec3.1/src/main.cpp
@@ -0,0 +1,169 @@
+//说明：修改顶点着色器及glDrawArray的参数，绘制三角形
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+#define numVAOs 1
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+void printShaderLog(GLuint shader)
+{
+	int len = 0;
+	int chWrittn = 0;
+	char* log;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0)
+	{
+		log = (char*)malloc(len);
+		glGetShaderInfoLog(shader, len, &chWrittn, log);
+		std::cout << "Shader Info Log: " << log << std::endl;
+		free(log);
+	}
+}
+
+void printProgramLog(int prog)
+{
+	int len = 0;
+	int chWrittn = 0;
+	char* log;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0)
+	{
+		log = (char*)malloc(len);
+		glGetProgramInfoLog(prog, len, &chWrittn, log);
+		std::cout << "Program Info Log: " << log << std::endl;
+		free(log);
+	}
+}
+
+bool checkOpenGLError()
+{
+	bool foundError = false;
+	int glErr = glGetError();
+	while (glErr != GL_NO_ERROR)
+	{
+		std::cout << "glError: " << glErr << std::endl;
+		foundError = true;
+		glErr = glGetError();
+	}
+	return foundError;
+}
+
+std::string readShaderSource(const char* filePath)
+{
+	std::string content;
+	std::ifstream fs(filePath, std::ios::in);
+	std::string line = "";
+	while (!fs.eof())
+	{
+		std::getline(fs, line);
+		content.append(line + "\n");
+	}
+	fs.close();
+	return content;
+}
+
+std::string getCurrentExecuteDir()
+{
+	char path[MAX_PATH];
+	GetModuleFileName(NULL, (LPSTR)path, sizeof(path));
+	std::string pathStr(path);
+	return pathStr.substr(0, pathStr.find_last_of("\\"));
+}
+
+GLuint createShaderProgram()
+{
+	std::string vShaderPathStr = getCurrentExecuteDir();
+	vShaderPathStr.append("\\shaders\\vertShader.glsl");
+	std::string fShaderPathStr = getCurrentExecuteDir();
+	fShaderPathStr.append("\\shaders\\fragShader.glsl");
+	std::string vShaderStr = readShaderSource(vShaderPathStr.c_str());
+	std::string fShaderStr = readShaderSource(fShaderPathStr.c_str());
+	const char* vshaderSource = vShaderStr.c_str();
+	const char* fshaderSource = fShaderStr.c_str();
+
+	GLint vertCompiled;
+	GLint fragCompiled;
+	GLint linked;
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(vShader, 1, &vshaderSource, NULL);
+	glShaderSource(fShader, 1, &fshaderSource, NULL);
+
+	glCompileShader(vShader);
+	checkOpenGLError();	//检查不出错误？
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+	if (vertCompiled != 1)
+	{
+		std::cout << "vertex compilation failed" << std::endl;
+		printShaderLog(vShader);
+	}
+
+	glCompileShader(fShader);
+	checkOpenGLError();
+	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+	if (fragCompiled != 1)
+	{
+		std::cout << "fragment compilation failed" << std::endl;
+		printShaderLog(fShader);
+	}
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+	glLinkProgram(vfProgram);
+	checkOpenGLError();
+	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+	if (linked != 1)
+	{
+		std::cout << "linking failed" << std::endl;
+		printProgramLog(vfProgram);
+	}
+	return vfProgram;
+}
+
+void init(GLFWwindow* window)
+{
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+void display(GLFWwindow* window, double currentTime)
+{
+	glUseProgram(renderingProgram);
+	//glPointSize(30.0f);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+int main()
+{
+	if (!glfwInit())
+	{
+		exit(EXIT_FAILURE);
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "window 1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK)
+	{
+		exit(EXIT_FAILURE);
+	}
+	glfwSwapInterval(1);
+	init(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
