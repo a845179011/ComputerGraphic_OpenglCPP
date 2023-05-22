5ee40fcce51b7817d9b04f1ea3d189d431e5f190
rzh <845179011@qq.com>
2023-05-22 12:45:08 +0800

    程序4.1

--- ch04/ch4.1/src/main.cpp
+++ ch04/ch4.1/src/main.cpp
@@ -1,4 +1,4 @@
-//说明：简单红色三角形
+//说明：简单红色立方体
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -13,8 +13,42 @@
 #include <Windows.h>
 
 #define numVAOs 1
+#define numVBOs 2
+
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
+
+void setupVertices()
+{
+	float vertextPositions[108] = {
+		-1.0f, 1.0f, -1.0f, -1.0f,-1.0f,-1.0f,1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,1.0f,1.0f,-1.0f,-1.0f,1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,-1.0f,
+		1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,-1.0f,
+		1.0f,-1.0f,1.0f,-1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,-1.0f,-1.0f,-1.0f,-1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,-1.0f,-1.0f,1.0f,-1.0f,-1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,1.0f,
+		-1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,
+		1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,-1.0f,
+	};
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertextPositions), vertextPositions, GL_STATIC_DRAW);
+}
+
 
 
 void init(GLFWwindow* window)
@@ -24,15 +58,43 @@ void init(GLFWwindow* window)
 	std::string fShaderPathStr = Utils::getCurrentExecuteDir();
 	fShaderPathStr.append("\\shaders\\fragShader.glsl");
 	renderingProgram = Utils::createShaderProgram(vShaderPathStr.c_str(), fShaderPathStr.c_str());
-	glGenVertexArrays(numVAOs, vao);
-	glBindVertexArray(vao[0]);
+	cameraX = 0.0f;
+	cameraY = 0.0f;
+	cameraZ = 8.0f;
+	cubeLocX = 0.0f;
+	cubeLocY = -2.0f;
+	cubeLocZ = 0.0f;
+	setupVertices();
 }
+
 void display(GLFWwindow* window, double currentTime)
 {
+	glClear(GL_DEPTH_BUFFER_BIT);
 	glUseProgram(renderingProgram);
-	//glPointSize(30.0f);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	mvMat = vMat * mMat;
+
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 }
+
 int main()
 {
 	if (!glfwInit())
