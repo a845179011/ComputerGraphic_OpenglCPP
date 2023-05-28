8b9e1549461e33c9a8f44ecdf4d8de5f351317e6
rzh <845179011@qq.com>
2023-05-28 15:24:13 +0800

    习题4.2--根据U,V,N设置视图矩阵

---
+++ ch04/ch4_exec4.2/src/main.cpp
@@ -0,0 +1,148 @@
+//说明：根据图3.13计算视图矩阵
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "Utils.h"
+#include <Windows.h>
+
+#define numVAOs 1
+#define numVBOs 2
+
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, tMat, rMat, mvMat;
+
+glm::mat4 buildV(const glm::vec3& U, const glm::vec3& V, const glm::vec3& N, const glm::vec3& cameraPos)
+{
+	glm::mat4 R{
+		U.x, V.x, N.x, 0.0f,
+		U.y, V.y, N.y, 0.0f,
+		U.z, V.z, N.z, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	};
+	glm::mat4 T{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		-cameraPos.x, -cameraPos.y, -cameraPos.z, 1.0f
+	};
+	return R * T;
+}
+
+void setupVertices()
+{
+	float vertextPositions[108] = {
+		-1.0f, 1.0f, -1.0f, -1.0f,-1.0f,-1.0f,1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,1.0f,1.0f,-1.0f,-1.0f,1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,-1.0f,
+		1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,-1.0f,
+		1.0f,-1.0f,1.0f,-1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,-1.0f,-1.0f,-1.0f,-1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,-1.0f,-1.0f,1.0f,-1.0f,-1.0f,1.0f,1.0f,
+		-1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,1.0f,
+		-1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,1.0f,
+		1.0f,1.0f,1.0f,-1.0f,1.0f,1.0f,-1.0f,1.0f,-1.0f,
+	};
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertextPositions), vertextPositions, GL_STATIC_DRAW);
+}
+
+void init(GLFWwindow* window)
+{
+	std::string vShaderPathStr = Utils::getCurrentExecuteDir();
+	vShaderPathStr.append("\\shaders\\vertShader.glsl");
+	std::string fShaderPathStr = Utils::getCurrentExecuteDir();
+	fShaderPathStr.append("\\shaders\\fragShader.glsl");
+	renderingProgram = Utils::createShaderProgram(vShaderPathStr.c_str(), fShaderPathStr.c_str());
+	cameraX = 8.0f;
+	cameraY = 0.0f;
+	cameraZ = 8.0f;
+	cubeLocX = 0.0f;
+	cubeLocY = 0.0f;
+	cubeLocZ = 0.0f;
+	setupVertices();
+}
+
+void display(GLFWwindow* window, double currentTime)
+{
+	glClear(GL_DEPTH_BUFFER_BIT);	//注释会导致每个曲面被清除，从而黑屏（P42）
+	glClear(GL_COLOR_BUFFER_BIT);	//注释将永久留下上一步的残影u
+	glUseProgram(renderingProgram);
+	
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+	tMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	mMat = tMat;
+	//vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	float sqrtVal = (float)std::sqrt(2.0) / 2.0;
+	glm::vec3 u{ sqrtVal, 0.0f, -sqrtVal };
+	glm::vec3 v{ 0.0f, 1.0f, 0.0f};
+	//glm::vec3 u{ 1.0f, 0.0f, 0.0f };
+	//glm::vec3 v{ 0.0f, 1.0f, 0.0f };
+	glm::vec3 n = glm::cross(u, v);
+
+	vMat = buildV(u, v, n, glm::vec3(cameraX, cameraY, cameraZ));
+	mvMat = vMat * mMat;
+
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+int main()
+{
+	if (!glfwInit())
+	{
+		exit(EXIT_FAILURE);
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "window 1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK)
+	{
+		exit(EXIT_FAILURE);
+	}
+	glfwSwapInterval(1);
+	init(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
---
+++ ch04/ch4_exec4.2/src/utils.h
@@ -0,0 +1,28 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <GL/glew.h>
+class Utils
+{
+public:
+
+	static void printShaderLog(GLuint shader);
+	
+
+	static void printProgramLog(int prog);
+	
+
+	static bool checkOpenGLError();
+	
+
+	static std::string readShaderSource(const char* filePath);
+	
+
+	static std::string getCurrentExecuteDir();
+	
+
+	static GLuint createShaderProgram(const char* vertexShaderPath, const char* flagShaderPath);
+	
+};
+#endif //!UTILS_H
