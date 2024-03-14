4035c86303caa0785742997c724b0e67dea2f037
rzh <845179011@qq.com>
2024-03-14 10:01:23 +0800

    测试glViewport

--- ch04/ch4.3/src/main.cpp
+++ ch04/ch4.3/src/main.cpp
@@ -87,19 +87,26 @@ void display(GLFWwindow* window, double currentTime)
 	glClear(GL_DEPTH_BUFFER_BIT);	//注释会导致每个曲面被清除，从而黑屏（P42）
 	glClear(GL_COLOR_BUFFER_BIT);	//注释将永久留下上一步的残影u
 	glUseProgram(renderingProgram);
-	
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+	GLuint colorLoc = glGetUniformLocation(renderingProgram, "u_color");
 
 	glfwGetFramebufferSize(window, &width, &height);
 	aspect = (float)width / (float)height;
-	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+	//pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+	//glViewport(0, 0, width/2, height/2);	//左下角
+	glViewport(0, 0, width, height);
+	pMat = glm::ortho(-3.0f, 3.0f, -3.0f, 3.0f, -10.0f, 10.0f);
 	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
 
 	mMat = glm::translate(glm::mat4(1.0), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
 	mvMat = vMat * mMat;
 	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+	glUniform4f(colorLoc, 1.0f, 0.0f, 0.0f, 1.0f);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
@@ -111,6 +118,7 @@ void display(GLFWwindow* window, double currentTime)
 	mvMat = vMat * mMat;
 	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+	glUniform4f(colorLoc, 1.0f, 1.0f, 0.0f, 1.0f);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
--- ch04/ch4.3/src/shaders/vertShader.glsl
+++ ch04/ch4.3/src/shaders/vertShader.glsl
@@ -4,10 +4,12 @@ layout(location=0) in vec3 position;
 
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
+uniform vec4 u_color;
 
 out vec4 varyingColor;
 void main(void) 
 {
 	gl_Position = proj_matrix*mv_matrix*vec4(position, 1.0);
-	varyingColor = vec4(position, 1.0) * 0.5 + vec4(0.5,0.5,0.5,0.5);
+	//varyingColor = vec4(position, 1.0) * 0.5 + vec4(0.5,0.5,0.5,0.5);
+	varyingColor=u_color;
 }
