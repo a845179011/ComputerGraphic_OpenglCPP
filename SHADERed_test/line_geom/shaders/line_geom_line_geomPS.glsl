2433baf3058cd20f91ac9d777994240586e99ed7
rzh <845179011@qq.com>
2024-03-19 10:29:26 +0800

    直线模糊效果

--- SHADERed_test/line_geom/shaders/line_geom_line_geomPS.glsl
+++ SHADERed_test/line_geom/shaders/line_geom_line_geomPS.glsl
@@ -37,21 +37,32 @@ void main(void)
 	vec2 c2=vec2( (pt3+pt4)/2.0);
 	vec2 cVec=c2-c1;
 	vec2 cDir=normalize( cVec);
-	check_value(pt1.y, 0.375, 0.38 );
+	//check_value(pt1.x, 0.161, 0.162);
+	//check_value(pt1.y, 0.454, 0.455);
+	//check_value(pt2.x, 0.283, 0.284);
+	//check_value(pt2.y, 0.454, 0.455);
+	//check_value(pt3.x, 0.161, 0.162);
+	//check_value(pt3.y, -0.365, -0.364);
+	//check_value(pt4.x, 0.283, 0.284);
+	//check_value(pt4.y, -0.365, -0.364);
 
 
 	// https://zhuanlan.zhihu.com/p/102068376
 	vec4 ndc = vec4( gl_FragCoord.xy/u_viewportSize * 2.0 - 1.0, gl_FragCoord.z * 2.0 - 1.0, 1.0 );
 	vec2 aVec=ndc.xy - c1;
 	
+	//check_value(ndc.x, 0.161, 0.283);
+	//check_value(ndc.y, -0.364, 0.454);
+
 	//vec2 aVec=gl_FragCoord.xy-c1;	
 	float projLength=dot(cVec, aVec) / length(cVec);
 	vec2 projPt=c1+cDir*projLength;
-	//float dist = distance(projPt, gl_FragCoord.xy);
 	float dist = distance(projPt, ndc.xy);
-	float feather = u_lineWidth * (2.0f / u_viewportSize.x);	
-	float factor=dist/(feather*2.0f);
-	
+
+	float feather = distance(pt1, pt2);
+	float factor = dist * 2.0 / feather;
+	//color = vec4(0.0f, 0.0f, 1.0f, 1.0f - factor);
+	color = vec4(0.0f, 1.0f - factor, 0.0f, 1.0f);
 
 	//color = u_color + vec4(0.0f, 0.0f, 1.0f, 1.0f - factor);
 	//color = u_color + vec4(0.0f, 0.0f, 1.0f, 1.0f - pt1.x);
